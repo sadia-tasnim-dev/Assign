c180521c2afcf666868f575b9310af739aa690ca
SadiaTasnim <sadiatasnim251@gmail.com>
2024-01-23 02:36:48 +0600

    hero section implemented

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// for hamburger menu
 const dropdown = document.getElementById("dropdown");
 const menu = document.getElementById("menu");
 let dropdownShown = false;
@@ -11,3 +12,43 @@ menu.addEventListener("click", function () {
     dropdownShown = true;
   }
 });
+// for hamburger menu
+
+// for carousel
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+let imagePosition = 1;
+
+function showImg() {
+  if (imagePosition === 1) {
+    img1.style.display = "block";
+    img2.style.display = "none";
+    img3.style.display = "none";
+  }
+  if (imagePosition === 2) {
+    img1.style.display = "none";
+    img2.style.display = "block";
+    img3.style.display = "none";
+  }
+  if (imagePosition === 3) {
+    img1.style.display = "none";
+    img2.style.display = "none";
+    img3.style.display = "block";
+  }
+}
+
+prev.addEventListener("click", function () {
+  imagePosition--;
+  if (imagePosition === 0) imagePosition = 3;
+  showImg();
+});
+
+next.addEventListener("click", function () {
+  imagePosition++;
+  if (imagePosition === 4) imagePosition = 1;
+  showImg();
+});
+// for carousel
